f90ce04aa47e98207e8142da9687e11628311d74
Mi Songhua <1512224016@qq.com>
2023-03-20 10:04:37 +0800

    Add list files viewing of whole project Add date of programming time

--- list_files.py
+++ list_files.py
@@ -1,7 +1,11 @@
 import os
+from datetime import date
 
 print(f"The files and folders in {os.getcwd()} are:")
 items = os.listdir('.')
 for item in items:
     prefix = "(d) " if os.path.isdir(item) else "(f) "
     print(f"{prefix}\t{item}")
+
+date_of_programming = date.today()
+print(f"date: {date_of_programming}")
